03c131ca618e744626bcd929341c9b271ea01fe6
“Hussam <hussamalkhaibari@gmail.comgit config --global user.email hussamalkhaibari@gmail.com>
2019-05-06 04:00:18 +0300

    finished sign in/out

--- starter-code/theme/js/app.js
+++ starter-code/theme/js/app.js
@@ -3,7 +3,25 @@ firebase.initializeApp(config);
 
 
 $(document).ready(function(){
-    
+                   $("#userList").hide();
+       $("#newArticle").click(function(event){
+        event.preventDefault();
+  		console.log("list clicked");
+
+  	});
+  $("#logout").click(function(event){
+        event.preventDefault();
+  		console.log("logout clicked");
+      firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+          $("#signIn").show();
+          $("#userList").hide();
+
+}).catch(function(error) {
+  // An error happened.
+          alert(error);
+});
+  	});
     $("#signIn").click(function(event){
         event.preventDefault();
   		console.log("button clicked");
@@ -38,8 +56,29 @@ $(document).ready(function(){
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        alert(errorCode+"/n"+errorMessage);
+
         // ...
       });
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+    // User is signed in.
+            var user = firebase.auth().currentUser;
+            var email = user.email;
+      		$("#popUp").addClass( "loader hidden" );
+            $("#signIn").hide();
+            $("#userList").text(email)
+            $("#userList").show();
+//           $("#inputEmailIn").attr("placeholder", "email").blur();
+
+//            $("#inputEmailIn").
+//            $("#inputPassIn").text("");
+
+  } else {
+    // No user is signed in.
+      console.log("No user is signed in");
+  }
+});
     });
       	$('#signInButt').click(function(event){
   		event.preventDefault();
@@ -56,12 +95,21 @@ $(document).ready(function(){
             firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-      
+      var user = firebase.auth().currentUser;
+    var email = user.email;
+      console.log(email);
       		$("#popUp").addClass( "loader hidden" );
+            $("#signIn").hide();
+            $("#userList").text(email)
+            $("#userList").show();
+//           $("#inputEmailIn").attr("placeholder", "email").blur();
+
+//            $("#inputEmailIn").
+//            $("#inputPassIn").text("");
 
   } else {
     // No user is signed in.
-      console.log("error");
+      console.log("No user is signed in");
   }
 });
     });
